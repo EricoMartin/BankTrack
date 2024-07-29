993293a9e733ca54b9aeceefe4ebc49359f96114
Eric Ibu <ericomartin.0785@gmail.com>
2024-07-29 19:42:26 +0100

    feat: write_tag

--- app/src/main/java/com/example/banktrack/MainActivity.kt
+++ app/src/main/java/com/example/banktrack/MainActivity.kt
@@ -4,27 +4,26 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.banktrack.ui.screens.WriteTagScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var textView = findViewById<TextView>(R.id.writeText)
-        var imageView = findViewById<ImageView>(R.id.writeImage)
+        val textView = findViewById<TextView>(R.id.writeText)
+        val imageView = findViewById<ImageView>(R.id.writeImage)
         moveToWriteScreen(textView, imageView)
     }
 
     private fun moveToWriteScreen(textView: TextView?, imageView: ImageView?) {
         textView!!.setOnClickListener {
-            Intent(this, WriteTagScreen::class.java)
+            val write = Intent(this, WriteTagScreen::class.java)
+            startActivity(write)
         }
         imageView!!.setOnClickListener {
-            Intent(this, WriteTagScreen::class.java)
+            val imgWrite = Intent(this, WriteTagScreen::class.java)
+            startActivity(imgWrite)
         }
     }
 
