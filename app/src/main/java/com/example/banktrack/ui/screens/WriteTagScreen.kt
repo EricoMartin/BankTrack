439f3c759aa5a2080d2b00c1b2665337a730c609
Ojiakor Michael <nicholausmichael51@gmail.com>
2024-07-30 04:11:37 +1200

    First two screens down

---
+++ app/src/main/java/com/example/banktrack/ui/screens/WriteTagScreen.kt
@@ -0,0 +1,16 @@
+package com.example.banktrack.ui.screens
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.banktrack.R
+
+class WriteTagScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.write_tag_screen)
+
+    }
+}
